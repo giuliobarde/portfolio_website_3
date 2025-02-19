2445844f415fc48f09e26cd3b920d79e4fd16d32
giuliobarde <giuliobarde@gmail.com>
2025-02-19 11:05:15 -0500

    first shape commit

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,7 +4,9 @@ import { useEffect, useRef } from "react";
 import { FC } from "react";
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import gsap from "gsap";
+import { gsap } from "gsap";
+import Bounded from "@/app/components/Bounded";
+import Shapes from "./Shapes"
 
 /**
  * Props for `Hero`.
@@ -52,20 +54,31 @@ const Hero: FC<HeroProps> = ({ slice }) => {
           },
         }
       );
+
+      tl.fromTo(
+        ".job-title",
+        {
+          y: 20,
+          opacity: 0,
+          scale: 1.2,
+        },
+        {
+          opacity: 1,
+          duration: 1,
+          scale: 1,
+          ease: "elastic.out(1,0.3)",
+        }
+      );
     }, component);
     return () => ctx.revert();
   }, []);
 
   const renderLetters = (name: KeyTextField, key: string) => {
     if (!name) return null;
-    const extraClasses =
-      key === "first"
-        ? "bg-gradient-to-tr from-yellow-600 via-yellow-200 to-yellow-600 bg-clip-text text-transparent"
-        : "";
     return name.split("").map((letter, index) => (
       <span
         key={index}
-        className={`name-animation name-animation-${key} inline-block opacity-0 ${extraClasses}`}
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
       >
         {letter}
       </span>
@@ -73,44 +86,46 @@ const Hero: FC<HeroProps> = ({ slice }) => {
   };
 
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <Shapes />
         <div className="col-start-1 md:row-start-1">
           <h1
-            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter whitespace-nowrap overflow-visible"
             aria-label={slice.primary.first_name + " " + slice.primary.last_name}
           >
-            <span className="block">
+            <span className="block whitespace-nowrap overflow-visible pr-4">
               {renderLetters(slice.primary.first_name, "first")}
             </span>
-            <span className="-mt-[-.2em] block text-slate-500">
+            <span className="block whitespace-nowrap overflow-visible -mt-[-.2em] text-slate-500">
               {renderLetters(slice.primary.last_name, "last")}
             </span>
           </h1>
           <span
             className="
+              job-title
               block bg-gradient-to-tr 
               from-yellow-500 
               via-yellow-200 
-              to-yellow-500 
+              to-yellow-600 
               bg-clip-text 
               text-2xl 
               font-bold 
               uppercase 
               tracking-[.2em] 
               text-transparent 
-              opacity-100 
+              opacity-0 
               md:text-4xl"
           >
             {slice.primary.tag_line}
           </span>
         </div>
       </div>
-    </section>
+    </Bounded>
   );
 };
 
