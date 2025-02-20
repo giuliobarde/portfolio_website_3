d39092c4b7f6ebf1afea728d0c334950cf2142fd
giuliobarde <giuliobarde@gmail.com>
2025-02-20 13:01:14 -0500

    About Me section commit

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,6 +1,11 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+import Button from "@/components/Button";
+import { PrismicNextImage } from "@prismicio/next";
+import Avatar from "./Avatar";
 
 /**
  * Props for `Biography`.
@@ -12,12 +17,34 @@ export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
  */
 const Biography: FC<BiographyProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for biography (variation: {slice.variation}) Slices
-    </section>
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-2 items-center">
+        {/* Left Column - Avatar */}
+        <div className="flex justify-center">
+          <Avatar 
+            image={slice.primary.avatar} 
+            className="max-w-sm"
+          />
+        </div>
+
+        {/* Right Column - Heading, Text, Button */}
+        <div className="flex flex-col space-y-6">
+          <Heading as="h1" size="xl">
+            {slice.primary.heading}
+          </Heading>
+          <div className="prose prose-xl prose-slate prose-invert">
+            <PrismicRichText field={slice.primary.description}/>
+          </div>
+          <Button 
+            linkField={slice.primary.button_link} 
+            label={slice.primary.button_text}
+          />
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
