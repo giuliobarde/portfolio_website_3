2a9c7429eb06898fb120b48bcf7204c76c87b838
giuliobarde <giuliobarde@gmail.com>
2025-02-27 12:51:25 -0500

    modal width fix

--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -30,7 +30,7 @@ type ProjectModalProps = {
             className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
             onClick={handleOutsideClick}
         >
-            <div className="bg-slate-700 p-8 rounded-lg shadow-lg max-w-xl w-full relative max-h-[80vh] sm:max-h-[90vh] overflow-scroll">
+            <div className="bg-slate-700 p-8 rounded-lg shadow-lg max-w-[90vw] sm:max-w-xl relative max-h-[80vh] sm:max-h-[90vh] overflow-scroll">
                 {/* Close Button */}
                 <button
                     onClick={onClose}
